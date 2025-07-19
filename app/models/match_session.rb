b242622eeaafd8a5cfa8f70bc10e2844fe45c041
Brooklyn-Dev <brooklynbaylisdev@gmail.com>
2025-07-19 16:25:53 +0100

    feat: generate edit_token before create MatchSession

--- app/models/match_session.rb
+++ app/models/match_session.rb
@@ -1,2 +1,10 @@
+require 'securerandom'
+
 class MatchSession < ApplicationRecord
+    before_create :generate_edit_token
+
+    private
+        def generate_edit_token
+            self.edit_token = SecureRandom.urlsafe_base64(16)
+        end
 end
